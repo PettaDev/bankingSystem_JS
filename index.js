051d8f80aec4fd0d7c2e871082d5f56c7b3fbafd
Gustavo Petta <160749630+PettaDev@users.noreply.github.com>
2024-04-23 14:46:23 -0300

    Arrumando

--- index.js
+++ index.js
@@ -9,47 +9,57 @@ function entrar() {
 	  alert("Senha incorreta, por favor tente novamente.");
 	  entrar();
 	}
-  }
+}
 
 function operacoes() {
-	let escolha = parseInt(prompt("Selecione uma opção 1.) Saldo 2.) Depósito 3.) Saque 4.) Sair"));
 	let saldo = 100.5;
-  
+	let continuar = true;
+
 	// SUGESTÃO: colocar um while para sempre que o usuário escolher uma opção voltar para as escolhas de operação, se o usuário escolher sair(opção 4) sai do looping.
+	while(continuar) {
+		let escolha = parseInt(prompt("Selecione uma opção 1.) Saldo 2.) Depósito 3.) Saque 4.) Sair"));
+		switch (escolha) {
+				
 
-	switch (escolha) {
+				case 1:
+					alert("O seu Saldo é: R$" + saldo + "0");
+					continuar  = window.confirm("Deseja continuar?");
+					if(continuar == false) break;
+					continue;
 		
-	  case 1:
-		alert("O seu Saldo é: R$" + saldo + "0");
-		break;
-	  case 2:
-		let valordep = parseFloat(prompt("Digite o valor do Deposito"));
-        let saldoapd = valordep + saldo;
+				case 2:
+					let valordep = parseFloat(prompt("Digite o valor do Deposito"));
+					let saldoapd = valordep + saldo;
+		
+					alert("Seu saldo após o deposito é igual a: " + saldoapd + "0");
+					continuar  = window.confirm("Deseja continuar?");
+					if(continuar == false) break;
+					continue;
+		
+				case 3:
+					let saque = parseFloat(prompt("Digite o valor do Saque: "));
+					let novosaldo = saldo - saque;
+			
+					if (saque >= saldo) {
+							alert("O valor da transferencia não pode ser maior que o saldo");
+					} else if (saque <= 0) {
+							alert("O valor da transferencia não pode ser negativo ou ZERO");
+					} else if (saque < saldo) {
+							alert("Seu saldo apos a transferência é igual a: " + novosaldo + "0" + " para a conta: " + conta);
+							operacoes()
+		
+					}
+					continuar  = window.confirm("Deseja continuar?");
+					if(continuar == false) break;
+					continue;
+		
+				case 4:
+					alert("Obrigado por acessar CASH-OUT Solutions. Te vejo na próxima!");
+					continuar  = false;
+					return entrar()
+				}
+				break;
+			} 
+		}
 
-	    alert("Seu saldo após o deposito é igual a: " + saldoapd + "0");
-		break;
-
-	  case 3:
-		let saque = parseFloat(prompt("Digite o valor do Saque: "));
-		let novosaldo = saldo - saque;
-  
-		  if (saque >= saldo) {
-				alert("O valor da transferencia não pode ser maior que o saldo");
-		  } else if (saque <= 0) {
-				alert("O valor da transferencia não pode ser negativo ou ZERO");
-		  } else if (saque < saldo) {
-				alert("Seu saldo apos a transferência é igual a: " + novosaldo + "0" + " para a conta: " + conta);
-				operacoes()
-
-		  }
-		break;
-
-	  case 4:
-		alert("Obrigado por acessar CASH-OUT Solutions. Te vejo na próxima!");
-		operacoes()
-		break;
-	}
-  }
-
-// Adicionei as chamada da funão "entrar" aqui em baixo.
 entrar(); 
